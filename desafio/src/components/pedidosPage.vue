f8afb28dc188cc0efe922a28fca7f27af92d8bee
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-10-21 15:16:09 -0300

    feat: components creation and work on welcomePage

---
+++ desafio/src/components/pedidosPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!--template-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pedidosPage',
+  // ...
+};
+</script>
+
+<style scoped>
+/* style */
+</style>
---
+++ desafio/src/components/welcomePage.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="welcome">
+    <h1>Bem vindo(a)</h1>
+    <p>Selecione uma seção para começar:</p>
+    <router-link to="/pessoasPage">Pessoas</router-link><br>
+    <router-link to="/produtosPage">Produtos</router-link><br>
+    <router-link to="/pedidosPage">Pedidos</router-link>
+  </section>
+</template>
+
+<style lang="scss">
+$backgroundColor: #0f7;
+$welcomeMarginTop: 50px;
+$linkMargin: 10px;
+$linkFontSize: 18px;
+$color: #04534f;
+
+body {
+  background-color: $backgroundColor;
+}
+
+.welcome {
+  text-align: center;
+  margin-top: $welcomeMarginTop;
+}
+
+router-link {
+  margin: $linkMargin;
+  font-size: $linkFontSize;
+  display: block;
+ 
+}
+
+a {
+
+  color: $color;
+  text-decoration: none;
+}
+</style>
+

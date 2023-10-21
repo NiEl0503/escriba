c8410467f60add28db038f98937fe5cb1a0bdf07
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-10-21 16:43:24 -0300

    Feat: Working on the people component

--- desafio/src/router.js
+++ desafio/src/router.js
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import WelcomePage from './components/welcomePage.vue';
-import PessoasPage from './components/pessoasPage.vue';
-import ProdutosPage from './components/produtosPage.vue';
-import PedidosPage from './components/pedidosPage.vue';
+import pessoasPage from './components/pessoasPage.vue';
+import produtosPage from './components/produtosPage.vue';
+import pedidosPage from './components/pedidosPage.vue';
 
 const routes = [
   { path: '/', component: WelcomePage },
-  { path: '/pessoas', component: PessoasPage },
-  { path: '/produtos', component: ProdutosPage },
-  { path: '/pedidos', component: PedidosPage },
+  { path: '/pessoasPage', component: pessoasPage },
+  { path: '/produtosPage', component: produtosPage },
+  { path: '/pedidosPage', component: pedidosPage },
 ];
 
 const router = createRouter({
